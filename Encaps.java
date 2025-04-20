aecc3af5f551a010231264306b5b3f02b5778ca9
Neeladhasan <neeladhasm@gmail.com>
2025-04-20 12:36:57 +0530

    feat:added coded to the file

--- Encaps.java
+++ Encaps.java
@@ -1,1 +1,23 @@
+class Moto{
+    private String password;
+    private String number;
 
+    public void Setdetails(String pass,String num){
+        password=pass;
+        number=num;
+    }
+
+    public void Getdetails(){
+        System.out.println("Password: "+password);
+        System.out.println("Phone: "+number);
+    }
+}
+
+class Main1{
+    public static void main(String args[]){
+     Moto m=new Moto();
+     m.Setdetails("Singam@1234", "8270056152");
+     m.Getdetails();
+
+    }
+}
